ffce70880a2207c7d9f82aecb95e02505678f215
Mark Part <Mark.Part@tthkee.onmicrosoft.com>
2024-04-02 22:48:25 +0300

    Добавьте файлы проекта.

---
+++ dzpython/dzpython.py
@@ -0,0 +1,22 @@
+# Tingimuslik operaator IF
+#1
+# def osta_pilet(vanus):
+#     if vanus < 0 or vanus > 100:
+#         return "Vigased andmed"
+#     elif vanus < 6:
+#         return "Tasuta"
+#     elif 6 <= vanus <= 14:
+#         return "Lastepilet"
+#     elif 15 <= vanus <= 65:
+#         return "Täispilet"
+#     else:
+#         return "Sooduspilet"
+
+# eesnimi = input("Sisesta Juku eesnimi: ")
+# vanus = int(input("Sisesta Juku vanus: "))
+
+# pilet = osta_pilet(vanus)
+# print(f"{eesnimi} pilet: {pilet}")
+
+#2
+
